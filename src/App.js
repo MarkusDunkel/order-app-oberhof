a022cfd5550f194ba73d1bc4a6f7a77cc9967442
Markus Dunkel <mrks.dunkel@gmail.com>
2022-10-25 16:14:45 +0200

    added input fields for quantity; some styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import './App.css';
 
 const App = () => {
 
@@ -8,7 +9,7 @@ const App = () => {
       "name": "Hirschfleisch",
       "quantity": "Kilo",
       "price": 20.4,
-      "selected": new Boolean(null), 
+      "selected": false, 
     },
     {
       "name": "Fisch",
@@ -20,7 +21,7 @@ const App = () => {
       "name": "Sirup",
       "quantity": "Flaschen",
       "price": 7.8,
-      "selected": true, 
+      "selected": false, 
     }
   ]);
 
@@ -36,7 +37,7 @@ const App = () => {
       res.push(i);
       };
     return res;}
-    newProducts[findId(newProducts, event.target.id)].selected = event.target.checked
+    newProducts[findId(newProducts, event.target.id.slice(0, -1))].selected = event.target.checked
     setProduct([ ...newProducts ]);
   };
 
@@ -57,24 +58,49 @@ const App = () => {
   );
 }
 
+const WithQuant = ({item, onSel}) => {
+  console.log(typeof item.name);
+  return (
+    <div className="sel-box" key={item.name}>
+      <div className="subsel-box">
+        <input id={item.name + "a"} type="checkbox" checked={item.selected} onChange={onSel} />
+        <label htmlFor={item.name + "a"}>{item.name}</label>
+      </div>
+      <div className="subsel-box">
+        <input id={item.name + "b"} type="text" />
+        <label htmlFor={item.name + "b"}>{item.quantity}</label>
+      </div>
+    </div>)
+}
+
+const WithoutQuant = ({item, onSel}) => {
+  return (
+    <div key={item.name}>
+      <div className="subsel-box">
+        <input id={item.name + "a"} type="checkbox" checked={item.selected} onChange={onSel} />
+        <label htmlFor={item.name + "a"}>{item.name}</label>
+      </div>
+    </div>)
+}
+
 const OrderList = ({products, onSel}) => {
   let temp_dict={};
   Object.assign(temp_dict, products);
   // console.log(products);
   return(
-    <form>
+    <form className="formi">
       {products.map(function (item) {
-        return (
-          <div key={item.name}>
-          <input id={item.name} type="checkbox" onChange={onSel} />
-          <label htmlFor={item.name}>{item.name}</label>
-          </div>
-        )   
+        if (item.selected) {
+          return <WithQuant item={item} onSel={onSel} />
+        }
+      return <WithoutQuant item={item} onSel={onSel} />
       }
       )}
     </form>
   );
 }
+
+
 
 const OrderOverview = () => {
   return(1);
